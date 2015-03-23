e55339046d5d1bdf59805202de053ae0bd00c5d0
Alexander Blythe <Alexander.blythe@gmail.com>
2015-03-23 16:32:35 -0400

    added Redistogo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener
+
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
 end
